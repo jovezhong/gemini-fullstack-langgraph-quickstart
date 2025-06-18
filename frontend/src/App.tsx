c014723d55ce8c3786ddb3246a101f8abe903991
Jove Zhong <jove@timeplus.io>
2025-06-18 15:29:21 -0700

    update title, footer and URL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,7 +24,7 @@ export default function App() {
   }>({
     apiUrl: import.meta.env.DEV
       ? "http://localhost:2024"
-      : "http://localhost:8123",
+      : "http://gemini.demo.timeplus.com:8123",
     assistantId: "agent",
     messagesKey: "messages",
     onUpdateEvent: (event: any) => {
